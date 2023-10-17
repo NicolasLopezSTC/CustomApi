928aa313f12404804f1b202b8b961514e0dc89f3
NicolasLopezSTC <143395793+NicolasLopezSTC@users.noreply.github.com>
2023-10-17 09:24:16 -0500

    Update router.js

--- src/router.js
+++ src/router.js
@@ -4,7 +4,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '@/views/HomePage.vue'
 
 const routes = [
-  { path: '/customapi/', name: 'Home', component: HomePage },
+  { path: '/CustomApi/', name: 'Home', component: HomePage },
   {
     path: '/monsters/:id',
     name: 'Cards',
