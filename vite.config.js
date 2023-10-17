7aee074be5128d96f85dac281315c26d8366b32c
NicolasLopezSTC <143395793+NicolasLopezSTC@users.noreply.github.com>
2023-10-17 09:17:52 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/customapi/',
+  base: '/Customapi/',
   plugins: [vue()],
   resolve: {
     alias: {
